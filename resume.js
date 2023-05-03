8ff21910c7240d2f505bd07e8c98431b1b1cef8b
Venkatesh T <81469290+venki00@users.noreply.github.com>
2023-05-03 11:40:00 +0530

    Update resume.js

---
+++ resume.js
@@ -0,0 +1,15 @@
+var resume = {
+    
+    "Name":"Venkatesh",
+    "Gender":"Male",
+    "DOB":"18.03.2000",
+    "Qualification":"DEEE",
+    "skills":"JavaScript,HTML,CSS",
+    "language":"English,Tamil",
+    "Nationality":"Indian",
+    "Country":"Tamilnadu",
+    "District":"Tuticorin",
+    "Mobile No":"9994455454",
+    "Mail Id":"Venkatesh@gmail.com"
+
+}
